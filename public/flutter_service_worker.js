9988ca3b50df5642518f53380494171e1bb8a12e
tl3abapp <tl3abapp@gmail.com>
2026-05-22 00:52:13 +0300

    Auto-enter app after account creation

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f37b8551ed6493da4851a3cb337642ed",
+const RESOURCES = {"flutter_bootstrap.js": "c9bed4c90410fcfa6a109168eb252a0b",
 "version.json": "499b7164c31a987975c4fdd1397baf31",
 "index.html": "431f3f8d041e5af4e5bcb0131fa9e6df",
 "/": "431f3f8d041e5af4e5bcb0131fa9e6df",
-"main.dart.js": "9b4b7dd66cae41f2db34f77f30d6d266",
+"main.dart.js": "558b965d2939a41f347a3530b16f8148",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "4aa9366546825150fe4034a565df8216",
 "icons/Icon-192.png": "94dc46e73b858b7cc8775eb13dfb3d33",
