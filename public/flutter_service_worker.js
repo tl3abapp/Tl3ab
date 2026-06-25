77b6d5883736214bc2898da2bebcc70106d3e322
tl3abapp <tl3abapp@gmail.com>
2026-06-25 16:48:23 +0300

    Add first game chat flow

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "29cc7be31ad66661e5fdf8481f572025",
+const RESOURCES = {"flutter_bootstrap.js": "63986bff3ddee958ef67d353bb140746",
 "version.json": "499b7164c31a987975c4fdd1397baf31",
 "index.html": "431f3f8d041e5af4e5bcb0131fa9e6df",
 "/": "431f3f8d041e5af4e5bcb0131fa9e6df",
-"main.dart.js": "284b5779b3c60a3a747d9ffd67aafc54",
+"main.dart.js": "4b1c70f37c923580801f604254206098",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "4aa9366546825150fe4034a565df8216",
 "icons/Icon-192.png": "94dc46e73b858b7cc8775eb13dfb3d33",
@@ -22,7 +22,7 @@ const RESOURCES = {"flutter_bootstrap.js": "29cc7be31ad66661e5fdf8481f572025",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/shaders/stretch_effect.frag": "40d68efbbf360632f614c731219e95f0",
 "assets/AssetManifest.bin": "1d71d1a7ecf3cd3a3136f95cd8fec4e8",
-"assets/fonts/MaterialIcons-Regular.otf": "4e786baee3fd5e15a4207c2b07f39e10",
+"assets/fonts/MaterialIcons-Regular.otf": "fbc71c417668093edb07ca3d72b95c09",
 "assets/assets/brand/tl3ab_wordmark.png": "8c136e02601cad82c77c2242eaffbe93",
 "assets/assets/brand/tl3ab_mark.png": "4498e8a11d7d966b36fdeb031f5d9d19",
 "assets/assets/icon/tl3b_icon.png": "fcbcac0dfafc07b818caccab653785f8",
